b5a17ea3fcb9f5f5b9e5163fe8a1b5dfb406bcd4
sabihDev <8203sabi.rahman@gmail.com>
2024-09-03 07:19:44 +0500

    animated hero component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './app.scss';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 const App = () => {
   return <div>
     <section id='Homepage'>
       <Navbar />
+      <Hero />
     </section>
     <section>Parallax</section>
     <section id='Services'>Services</section>
