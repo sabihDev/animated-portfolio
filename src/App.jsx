e17495fdb64fdd3647612f1b32ca4411ab42b18c
sabihDev <8203sabi.rahman@gmail.com>
2024-09-03 06:26:39 +0500

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './app.scss';
+import Navbar from './components/Navbar/Navbar';
 const App = () => {
   return <div>
-    <section>Hero</section>
+    <section id='Homepage'>
+      <Navbar />
+    </section>
     <section>Parallax</section>
-    <section>Services</section>
+    <section id='Services'>Services</section>
     <section>Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section>Contact</section>
+    <section id='Contact'>Contact</section>
   </div>;
 };
 
