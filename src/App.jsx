15bc2ec47896560a6d004c844d72de51f3e98d39
sabihDev <8203sabi.rahman@gmail.com>
2024-09-03 07:52:28 +0500

    animated parallex done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './app.scss';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
+import Parallex from './components/Parallex/Parallex';
 const App = () => {
   return <div>
     <section id='Homepage'>
       <Navbar />
       <Hero />
     </section>
-    <section>Parallax</section>
-    <section id='Services'>Services</section>
+    <section id='Services'><Parallex type='services'/></section>
+    <section>Services</section>
+    <section id='Portfolio'><Parallex type='portfolio'/></section>
     <section>Parallax</section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
